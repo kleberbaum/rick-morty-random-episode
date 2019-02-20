ac40f84da76bceb39567d7da00a21d017fcfc150
Florian Kleber <kleberbaum@erebos.xyz>
2019-02-20 10:06:32 +0100

    test2

---
+++ build/precache-manifest.689a1710f0f720305e7313e1efb7fc67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d8ceb912e8b437fcd8a",
+    "url": "/static/css/main.478203ea.chunk.css"
+  },
+  {
+    "revision": "0d8ceb912e8b437fcd8a",
+    "url": "/static/js/main.0d8ceb91.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a4298c34994359149f86",
+    "url": "/static/js/2.a4298c34.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6d19485948665e9f2045771c2eda8d74",
+    "url": "/index.html"
+  }
+];
